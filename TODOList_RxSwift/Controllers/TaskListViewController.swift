9a785c364934a0d5c7d4d3bf7b495f7c4aae2367
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-03 17:32:44 +0200

    Designing the Task List Screen

---
+++ TODOList_RxSwift/Controllers/TaskListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  TaskListViewController.swift
+//  TODOList_RxSwift
+//
+//  Created by mohamed  habib on 03/08/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    
+    @IBOutlet weak var perioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        return cell
+    }
+    
+}
+
+
